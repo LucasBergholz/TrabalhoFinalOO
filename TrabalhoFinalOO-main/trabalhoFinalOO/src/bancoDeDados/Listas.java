22b88b82da552fa8f2e630bfdffa3c790e054bef
Guilherme Costa <guilhermercosta1@gmail.com>
2022-08-12 08:28:55 -0300

    Fluminense Vs Santos

--- TrabalhoFinalOO-main/trabalhoFinalOO/src/bancoDeDados/Listas.java
+++ TrabalhoFinalOO-main/trabalhoFinalOO/src/bancoDeDados/Listas.java
@@ -7,28 +7,29 @@ import modelo.*;
 public class Listas {
 	//Criando ArrayList dos times;
 		private static final ArrayList<Time> times = new ArrayList<>();
+		private static final ArrayList<Partida> partidas = new ArrayList<>();
 		
 		public void inicializarTimes() {
-			times.add(new Time("AMERICA-MG", "#00FF00", "#FFFFFF", Estadios.INDEPENDENCIA));
-			times.add(new Time("ATHLETICO-PR", "#FF0000", "#000000", Estadios.ARENA_DA_BAIXADA));
-			times.add(new Time("ATLETICO-GO", "#FFFFFF", "#000000", Estadios.ANTONIO_ACCIOLY));
-			times.add(new Time("ATLETICO-MG", "#000000", "#FFFFFF", Estadios.MINEIRAO));
-			times.add(new Time("AVAI", "#0000FF", "#FFFFFF", Estadios.RESSACADA));
-			times.add(new Time("BOTAFOGO", "#000000", "#FFFFFF", Estadios.NILTON_SANTOS));
-			times.add(new Time("CEARA", "#000000", "#FFFFFF", Estadios.CASTELAO));
-			times.add(new Time("CORINTHIANS", "#FFFFFF", "#000000", Estadios.NEO_QUIMICA_ARENA));
-			times.add(new Time("CORITIBA", "#008000", "#FFFFFF", Estadios.COUTO_PEREIRA));
-			times.add(new Time("CUIABA", "#008000", "#FFEE05", Estadios.ARENA_PANTANAL));
-			times.add(new Time("FLAMENGO", "#FF0000", "#000000", Estadios.MARACANA));
-			times.add(new Time("FLUMINENSE", "#831D1C", "#146b02", Estadios.MARACANA));
-			times.add(new Time("FORTALEZA", "#0000FF", "#FF0000", Estadios.CASTELAO));
-			times.add(new Time("GOIAS", "#008000", "#FFFFFF", Estadios.SERRINHA));
-			times.add(new Time("INTERNACIONAL", "#FF0000", "#FFFFFF", Estadios.BEIRA_RIO));
-			times.add(new Time("JUVENTUDE", "#008000", "#FFFFFF", Estadios.ALFREDO_JACONI));
-			times.add(new Time("PALMEIRAS", "#008000", "#FFFFFF", Estadios.ALLIANZ_PARQUE));
-			times.add(new Time("RED BULL BRAGANTINO", "#FFFFFF", "#FF0000", Estadios.NABI_ABI_CHEDID));
-			times.add(new Time("SANTOS", "#0000000", "#FFFFFF", Estadios.VILA_BELMIRO));
-			times.add(new Time("SAO PAULO", "#FF0000", "#FFFFFF", Estadios.MORUMBI));
+			times.add(new Time("AMERICA-MG", "#00FF00", "#FFFFFF", Estadios.INDEPENDENCIA)); //0
+			times.add(new Time("ATHLETICO-PR", "#FF0000", "#000000", Estadios.ARENA_DA_BAIXADA));//1
+			times.add(new Time("ATLETICO-GO", "#FFFFFF", "#000000", Estadios.ANTONIO_ACCIOLY));//2
+			times.add(new Time("ATLETICO-MG", "#000000", "#FFFFFF", Estadios.MINEIRAO));//3
+			times.add(new Time("AVAI", "#0000FF", "#FFFFFF", Estadios.RESSACADA));//4
+			times.add(new Time("BOTAFOGO", "#000000", "#FFFFFF", Estadios.NILTON_SANTOS));//5
+			times.add(new Time("CEARA", "#000000", "#FFFFFF", Estadios.CASTELAO));//6
+			times.add(new Time("CORINTHIANS", "#FFFFFF", "#000000", Estadios.NEO_QUIMICA_ARENA));//7
+			times.add(new Time("CORITIBA", "#008000", "#FFFFFF", Estadios.COUTO_PEREIRA));//8
+			times.add(new Time("CUIABA", "#008000", "#FFEE05", Estadios.ARENA_PANTANAL));//9
+			times.add(new Time("FLAMENGO", "#FF0000", "#000000", Estadios.MARACANA));//10
+			times.add(new Time("FLUMINENSE", "#831D1C", "#146b02", Estadios.MARACANA));//11
+			times.add(new Time("FORTALEZA", "#0000FF", "#FF0000", Estadios.CASTELAO));//12
+			times.add(new Time("GOIAS", "#008000", "#FFFFFF", Estadios.SERRINHA));//13
+			times.add(new Time("INTERNACIONAL", "#FF0000", "#FFFFFF", Estadios.BEIRA_RIO));//14
+			times.add(new Time("JUVENTUDE", "#008000", "#FFFFFF", Estadios.ALFREDO_JACONI));//15
+			times.add(new Time("PALMEIRAS", "#008000", "#FFFFFF", Estadios.ALLIANZ_PARQUE));//16
+			times.add(new Time("RED BULL BRAGANTINO", "#FFFFFF", "#FF0000", Estadios.NABI_ABI_CHEDID));//17
+			times.add(new Time("SANTOS", "#0000000", "#FFFFFF", Estadios.VILA_BELMIRO));//18
+			times.add(new Time("SAO PAULO", "#FF0000", "#FFFFFF", Estadios.MORUMBI));//19
 		}
 
 		public void inicializarJogadores() {
@@ -94,6 +95,13 @@ public class Listas {
 			times.get(19).addJogador("MARCOS GUILHERME", Posicao.PONTA);
 		}
 		
+		public void inicializarPartidas() {
+			//Flu VS Santos
+			partidas.add(new Partida(times.get(11), times.get(19), Estadios.MARACANA));
+			partidas.get(0).finalizarPartida(0, 0, 01);
+			
+			//Atletico VS 
+		}
 		public ArrayList<Time> getTimes() {
 			return times;
 		}
