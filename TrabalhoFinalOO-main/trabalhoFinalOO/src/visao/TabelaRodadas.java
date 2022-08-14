f8b686c159760104595757fa44d32da6a3155eae
Guilherme Costa <guilhermercosta1@gmail.com>
2022-08-14 08:50:06 -0300

    Ajustando o Dimensionamento da tabela de Rodada

--- TrabalhoFinalOO-main/trabalhoFinalOO/src/visao/TabelaRodadas.java
+++ TrabalhoFinalOO-main/trabalhoFinalOO/src/visao/TabelaRodadas.java
@@ -7,6 +7,7 @@ import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
@@ -14,6 +15,8 @@ import javax.swing.SwingConstants;
 import javax.swing.JSeparator;
 import javax.swing.JLabel;
 import java.awt.SystemColor;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -29,6 +32,7 @@ import javax.swing.SpringLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.Cursor;
+import java.awt.Dimension;
 
 public class TabelaRodadas extends JFrame {
 
@@ -51,6 +55,7 @@ public class TabelaRodadas extends JFrame {
 	public TabelaRodadas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 750, 600);
+		setResizable(false);
 		painelConteudo = new JPanel();
 		painelConteudo.setBackground(new Color(0, 0, 128));
 		painelConteudo.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -94,69 +99,82 @@ public class TabelaRodadas extends JFrame {
 		List<JSeparator> separadores = new ArrayList<>();
 		List<JLabel> labels = new ArrayList<>();
 		Box caixaVertical = Box.createVerticalBox();
-		caixaVertical.setSize(700, 154*11);
+		caixaVertical.setSize(736, 1690);
 		painelConteudo.add(caixaVertical);
-		caixaVertical.setLocation(10, 175);
+		caixaVertical.setLocation(0, 179);
+		
+		/*JLabel lblNewLabel = new JLabel(Integer.toString(rod));
+		lblNewLabel.setBounds(85, 24, 45, 13);
+		lblNewLabel.setForeground(Color.WHITE);
+		painelConteudo.add(lblNewLabel);*/
 		
 		for (int i = 0; i < numPartidas-1; i++) {
-			
-			Box caixaHorizontal = Box.createHorizontalBox();
-			
-			JPanel panel_1 = new JPanel();
-			panel_1.setSize(622, 154);
-			panel_1.setLayout(null);
-			
-			if(i ==1) {
-				panel_1.setBackground(Color.cyan);
-			}
-			JLabel lblTimeCasa = new JLabel(menu.getBrasileirao().getPartidas().get(i).getTimeCasa().getNome());
-			lblTimeCasa.setBounds(95, 69, 103, 13);
-			panel_1.add(lblTimeCasa);
-			
-			JLabel lblTimeFora = new JLabel(menu.getBrasileirao().getPartidas().get(i).getTimeFora().getNome());
-			lblTimeFora.setBounds(505, 69, 65, 13);
-			panel_1.add(lblTimeFora);
-			
-			JLabel lblEstadio = new JLabel(menu.getBrasileirao().getPartidas().get(i).getEstadio().toString());
-			lblEstadio.setBounds(275, 131, 95, 13);
-			panel_1.add(lblEstadio);
-			
-			JLabel lblX = new JLabel("X");
-			lblX.setFont(new Font("Tahoma", Font.PLAIN, 31));
-			lblX.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
-			lblX.setBounds(302, 69, 45, 13);
-			panel_1.add(lblX);
-			
-			JLabel lblGolsCasa = new JLabel(Integer.toString(menu.getBrasileirao().getPartidas().get(i).getGolsCasa()));
-			lblGolsCasa.setBounds(204, 69, 45, 13);
-			panel_1.add(lblGolsCasa);
-			
-			JLabel lblGolsFora = new JLabel(Integer.toString(menu.getBrasileirao().getPartidas().get(i).getGolsFora()));
-			lblGolsFora.setBounds(388, 69, 45, 13);
-			panel_1.add(lblGolsFora);
-			
-			JSeparator separator = new JSeparator();
-			separator.setBounds(0, 130, 736, 12);
-			
-			
-			//Adcionando as Listas
-			labels.add(lblTimeCasa);
-			labels.add(lblTimeFora);
-			labels.add(lblEstadio);
-			labels.add(lblEstadio);
-			labels.add(lblGolsCasa);
-			labels.add(lblGolsFora);
-			labels.add(lblX);
-			painelPartidas.add(panel_1);
-			caixaHorizontal.add(panel_1);
-			caixaVertical.add(caixaHorizontal);
-			separadores.add(separator);
-			caixaHorizontal.add(separator);
-			caixaVertical.add(separator);
-			
+			if(menu.getBrasileirao().getPartidas().get(i).getRodada() == rod) {
+				Box caixaHorizontal = Box.createHorizontalBox();
+				
+				JPanel panel_1 = new JPanel();
+				panel_1.setSize(622, 154);
+				panel_1.setBackground(new Color(0x483D8B));
+				panel_1.setLayout(null);
+				
+				JLabel lblTimeCasa = new JLabel(menu.getBrasileirao().getPartidas().get(i).getTimeCasa().getNome());
+				lblTimeCasa.setBounds(95, 69, 103, 13);
+				panel_1.add(lblTimeCasa);
+				
+				JLabel lblTimeFora = new JLabel(menu.getBrasileirao().getPartidas().get(i).getTimeFora().getNome());
+				lblTimeFora.setBounds(505, 69, 103, 13);
+				panel_1.add(lblTimeFora);
+				
+				JLabel lblEstadio = new JLabel(menu.getBrasileirao().getPartidas().get(i).getEstadio().toString());
+				lblEstadio.setBounds(275, 131, 95, 13);
+				panel_1.add(lblEstadio);
+				
+				JLabel lblX = new JLabel("X");
+				lblX.setFont(new Font("Tahoma", Font.PLAIN, 31));
+				lblX.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+				lblX.setBounds(302, 69, 45, 13);
+				panel_1.add(lblX);
+				
+				JLabel lblGolsCasa = new JLabel(Integer.toString(menu.getBrasileirao().getPartidas().get(i).getGolsCasa()));
+				lblGolsCasa.setBounds(204, 69, 45, 13);
+				panel_1.add(lblGolsCasa);
+				
+				JLabel lblGolsFora = new JLabel(Integer.toString(menu.getBrasileirao().getPartidas().get(i).getGolsFora()));
+				lblGolsFora.setBounds(388, 69, 45, 13);
+				panel_1.add(lblGolsFora);
+				
+				JSeparator separator = new JSeparator();
+				separator.setMaximumSize(new Dimension(3000, 800));
+				separator.setBackground(new Color(0x483D8B));
+				separator.setBounds(0, 130, 736, 12);
+				
+				//Adcionando as Listas
+				labels.add(lblTimeCasa);
+				labels.add(lblTimeFora);
+				labels.add(lblEstadio);
+				labels.add(lblEstadio);
+				labels.add(lblGolsCasa);
+				labels.add(lblGolsFora);
+				labels.add(lblX);
+				painelPartidas.add(panel_1);
+				caixaHorizontal.add(panel_1);
+				caixaVertical.add(caixaHorizontal);
+				separadores.add(separator);
+				caixaHorizontal.add(separator);
+				caixaVertical.add(separator);
+			}	
 		}
 		
-		
+		/*JScrollBar scrollBar = new JScrollBar();
+		painelConteudo.add(scrollBar);
+		scrollBar.setBounds(719, 0, 17, 575);
+		scrollBar.addAdjustmentListener(new AdjustmentListener() {  
+		    public void adjustmentValueChanged(AdjustmentEvent e) {  
+		    	titulo.setBounds(0, 0-scrollBar.getValue(), 751, 100);
+		    	caixaVertical.setBounds(40, 100-scrollBar.getValue(), 650, 525);
+		    	panel.setBounds(10, 35-scrollBar.getValue(), 85, 21);
+		    }
+		 });*/
 		
 		
 	}
